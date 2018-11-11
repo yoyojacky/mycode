9d11144a9f4ca442c5779f863649bdd9c5b9a3c2
yoyojacky <yoyojacky@users.noreply.github.com>
2018-11-11 13:22:04 +0800

    readtemp.c has been created
    just read temp in c

---
+++ readtemp.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int main(void)
+{
+        float temp;
+        char str[999];
+        FILE *file;
+        file = fopen("/sys/class/thermal/thermal_zone0/temp", "r");
+        if(file){
+                while(fscanf(file, "%s", str) !=EOF)
+                        printf("%.2f\n",strtod(str, NULL)/1000);
+                fclose(file);
+        }
+        return 0;
+}
