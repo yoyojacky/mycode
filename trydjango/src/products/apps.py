5347d1f965fac6e77f1df6081202c7115c47fa11
yoyojacky <yoyojacky2009@gmail.com>
2019-08-27 19:03:28 +0800

    update and delete django
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- trydjango/src/products/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ProductsConfig(AppConfig):
-    name = 'Products'
-
-    class Meta:
-        verbose_name = '仓库管理'
-        verbose_name_plural = '仓库管理'
