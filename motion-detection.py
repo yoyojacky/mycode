a42d6822cf4782a34de5d2512829a441975fbe2f
yoyojacky <yoyojacky2009@gmail.com>
2020-01-14 19:24:12 +0800

    update a little bit, drawcontour as rectangle
    Signed-off-by: yoyojacky <yoyojacky2009@gmail.com>

--- motion-detection.py
+++ motion-detection.py
@@ -17,11 +17,13 @@ while cap.isOpened():
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         
-        if cv2.contourArea(contour) < 700:
+        if cv2.contourArea(contour) < 900:
             continue
         else:
             cv2.drawContours(frame1, contours, -1, (0, 255, 0), 2)
-    
+        
+        cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        cv2.putText(frame1, "Status: {}".format("Movement"), (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 3)
     cv2.imshow("motion-detection", frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
